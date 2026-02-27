e57c478a77406c180ce5cb6a5ccba9bf755439e3
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-02-27 20:38:11 +0700

    Logger msg info

---
+++ Backend/src/utils/logger.js
@@ -0,0 +1,22 @@
+export const logger ={
+    info: (msg, data=null)=>{
+        console.log(`Information: ${msg}`, data ?? '');
+    },
+    error: (msg, data=null) =>{
+        console.error(`Error: ${msg}`, data ?? '');
+    },
+    success: (msg, data = null) => {
+        console.log(`Success: ${msg}`, data ?? '');
+    },
+    request: (method, url, data = null)=>{
+       console.log(`Request: ${method} ${url}`, data ?? ''); 
+    },
+    response: (status, data= null)=>{
+        console.log(`Response: ${status}`, data ?? '');  
+    },
+    warn: (msg, data = null) => {
+        console.warn(`Warning: ${msg}`, data ?? '');
+    }
+}
+
+export default logger;
